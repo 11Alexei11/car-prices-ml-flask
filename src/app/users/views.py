0386984c3693faf1b7b86da0ccef3a90ffb123ba
11Alexei11 <lesha.borovik@gmail.com>
2024-02-17 21:39:31 +0100

    merge branch feature/app into feature/make_registration_page

--- src/app/users/views.py
+++ src/app/users/views.py
@@ -1,9 +1,33 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 
 users_bp = Blueprint("users", import_name=__name__, static_folder='static', template_folder='templates')
 
 
-@users_bp.route('/user/login', methods=['GET'])
+@users_bp.route('/user/login/form', methods=['GET'])
 def get_login_page():
     return render_template('login_form.html')
+
+
+# @users_bp.route('/user/registration', methods=['POST'])
+# def handle_register_action():
+#     if request.method.POST:
+#         name = request.method.POST['name']
+#         email = request.method.POST['email']
+#         password = request.method.POST['password']
+
+#         if not User.objects.filter(NAME=name, EMAIL=email, PASSWORD=password).exists():
+#             user = User(
+#                 NAME=name,
+#                 EMAIL=email,
+#                 PASSWORD=password
+#             )
+#             user.save()
+#             print('new user')
+#             return redirect('login-form')
+#         else:
+#             print('existing user')
+#             return redirect('registration-form')
+#     else:
+#         print(request.GET)
+#         raise Exception('this requesregister_statust should be post')
